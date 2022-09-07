ff69494547e97f9206ec6cfb0e15295a336e9b6e
stephanieLQ <stephanielunaquevedo@gmail.com>
2022-09-06 23:54:54 -0400

    cambio de server con mongo

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const app = require('./app');
 
 const port = process.env.PORT;
+const dbUrl = process.env.MONGO;
+const mongoose = require('mongoose');
 
+mongoose.connect(process.env.MONGO)
+  .then((con) => {
+    console.log('primer coneccion a MONGO');
+  });
 app.listen(port, () => {
   console.log(`Servidor iniciado en puerto ${port}`);
  });
